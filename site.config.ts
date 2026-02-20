0bb5b19e05e88309720ba05b9f80ba6bb0d1a519
FireTable <firetable@foxmail.com>
2026-02-21 01:09:17 +0800

    chore: switch to database-driven slugs and remove manual override

--- site.config.ts
+++ site.config.ts
@@ -43,9 +43,7 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: {
-    '/lab-log-2026-02-20': '30dd95e228bd81fbb1f1ef0319460601'
-  },
+  pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
